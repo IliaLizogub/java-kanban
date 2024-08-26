319c5332491621b153140259f2ab30215d56ff92
Ilia Lizogub <lizogub.ilya@yandex.ru>
2024-08-26 12:12:26 +0300

    фз 4 спринт

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,30 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Поехали!");
+
+        TaskManager taskManager = new TaskManager();
+
+        Task cutDog = new Task("Подстричь собаку", "Ножницами");
+        Task washFloorCreated = taskManager.addTask(cutDog);
+        System.out.println(washFloorCreated);
+        Task cutDogToUpdate = new Task(cutDog.getId(), "Нужно подстричь собаку", "Можно уже машинкой",
+                Status.IN_PROGRESS);
+        Task cutDogUpdate = taskManager.updateTask(cutDogToUpdate);
+        System.out.println(cutDogUpdate);
+
+
+        Epic constructionVillage = new Epic("Начать освоение загородного участка", "За лето");
+        taskManager.addEpic(constructionVillage);
+        System.out.println(constructionVillage);
+        Subtask constructionVillageSubtask1 = new Subtask("Установить забор", "Заказать в фирме Заров.ру",
+                constructionVillage.getId());
+        Subtask constructionVillageSubtask2 = new Subtask("Построить баню", "Строю сам по каркасной технологии",
+                constructionVillage.getId());
+        taskManager.addSubtask(constructionVillageSubtask1);
+        taskManager.addSubtask(constructionVillageSubtask2);
+        System.out.println(constructionVillage);
+        constructionVillageSubtask2.setStatus(Status.DONE);
+        taskManager.updateSubtask(constructionVillageSubtask2);
+        System.out.println(constructionVillage);
     }
 }
